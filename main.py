ca031122e4a46477f44ce816b04b78e51c7eb26b
Varun Lobo <114509328+varunlobo@users.noreply.github.com>
2023-01-11 20:44:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,22 +10,28 @@ header = st.container()
 EDA = st.container()
 modeltraining = st.container()
 
+@st.cache
+def get_data():
+    
+    df=pd.read_csv("employee_data.csv")
+    
+    return df
 
 
 with header:
     
-    st.title("Building a dynamic webpage using Streamlit in Python.")
-    st.subheader("Streamlit is an open source app framework in Python language. \
+    st.title("Implementation of ML classification algorithms using Streamlit in Python.")
+    st.markdown("Streamlit is an open source app framework in Python language. \
             It helps us create web apps for data science and machine learning.\
             It is compatible with major Python libraries such as scikit-learn,\
             Keras, PyTorch, NumPy, pandas, Matplotlib etc")
     
 
 with EDA:
-    st.header("Let us perform Exploratory Data Analysis of our dataset")
+    st.header("Exploratory Data Analysis of our dataset")
     st.text("Dataset for illustration was downloaded from Kaggle.com")
     
-    df=pd.read_csv("employee_data.csv")
+    df=get_data()
     st.subheader("A view of our dataset")
     row = st.slider('Display Rows', 0, 50, 5)
     
@@ -63,20 +69,26 @@ with EDA:
     st.pyplot(fig)
     
 with modeltraining:
-    st.header("In this section we will divide our dataset into training \
+    
+    st.header("Let us dive into implementation of our ML models.")
+    st.subheader("We will implement three different models to compare from. \
+             Support Vector Machines (SVM), Decision Trees (DT) and \
+                 Logistic Regression")
+    
+    st.subheader("Let's first divide our dataset into training \
               and testing datasets")
     
     split = st.slider('Input % for dividing dataset', 0, 100, 50)
     
     split=split/100
     
-    st.header("Determine max depth of Decision Tree")
-    maxdepth = st.slider('Max depth of decision tree', 0, 15, 5)
+
     
     y=df['Attrition'].map({'Yes':1, 'No':0})
     
     x=df[['Age', 'NumCompaniesWorked', 'JobSatisfaction' , 'PerformanceRating' ,'MonthlyIncome', 'JobLevel' , 'DistanceFromHome']]
     
+    #Standard Scalar for Normalization
     from sklearn.preprocessing import MinMaxScaler
     scaler = MinMaxScaler()
     X = scaler.fit_transform(x)
@@ -84,22 +96,67 @@ with modeltraining:
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=split, random_state=0)
     
+ 
+    st.header("Machine Learning (Classification) Algorithm Selection")
+    mlselect=st.selectbox("",options=['Support Vector Machines (SVM)', 'Decision Trees (DT)', 'Logistic Regression'])
+    
+    colA, colB = st.columns(2)
+    
+    
+    #default algo = DT
     
     from sklearn.tree import DecisionTreeClassifier
-    dtc = DecisionTreeClassifier(criterion='gini', max_depth=maxdepth)
+    dtc = DecisionTreeClassifier(criterion='gini', max_depth=5)
     dtc.fit(X_train,y_train)
-    y_predicted = dtc.predict(X_test)
+    y_predicted = dtc.predict(X_test)  
+
     
-    st.header("Error Metrics to Determine the accuracy of our ML model")
+    if mlselect=='Decision Trees (DT)':
+        
+        # Decision Tree
+        colA.subheader("Determine max depth of Decision Tree")
+        maxdepth = colA.slider('Max depth of decision tree', 0, 15, 5)
+        
+        colA.subheader("Hyper Parameter Selection")
+        criteriaDT=colA.selectbox("",options=['gini','entropy','log_loss'])
+        
+        from sklearn.tree import DecisionTreeClassifier
+        dtc = DecisionTreeClassifier(criterion=criteriaDT, max_depth=maxdepth)
+        dtc.fit(X_train,y_train)
+        y_predicted = dtc.predict(X_test)  
+    
+    if mlselect=='Logistic Regression':
+        
+        maxiter = colA.slider('Max iteration', 0, 100, 50)
+        #Building logistic regression model
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression(max_iter=maxiter)
+        model.fit(X_train,y_train)
+        y_predicted = model.predict(X_test)
+        
+    if mlselect=='Support Vector Machines (SVM)':
+         
+        colA.subheader("Hyper Parameter Selection")
+        kernelsvm=colA.selectbox("", options=["linear", "poly", "rbf", "sigmoid"])
+         
+        from sklearn.svm import SVC #"Support vector classifier"
+        model = SVC(kernel=kernelsvm)
+        model.fit(X_train,y_train)
+        y_predicted = model.predict(X_test)
+         
+         
+        
+    # Error Metrics
+    colB.subheader("Error Metrics to Determine the accuracy of our ML model")
     
     from sklearn import metrics 
-    st.subheader("Mean Absolute Error")
-    st.write(metrics.mean_absolute_error(y_test, y_predicted))
+    colB.subheader("Mean Absolute Error")
+    colB.write(metrics.mean_absolute_error(y_test, y_predicted))
 
-    st.subheader("Root Mean Sq Error")
-    st.write(np.sqrt(metrics.mean_squared_error(y_test, y_predicted)))
+    colB.subheader("Root Mean Sq Error")
+    colB.write(np.sqrt(metrics.mean_squared_error(y_test, y_predicted)))
     
-    st.subheader("R2 Score:")
-    st.write(dtc.score(X_test, y_test))
+    colB.subheader("R2 Score:")
+    colB.write(dtc.score(X_test, y_test))
     
     
